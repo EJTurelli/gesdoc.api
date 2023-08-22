5ae4c18012f3cb6e0f8a997ddae45d7237efc35e
Ernesto Turelli <ejturelli@yahoo.com>
2023-08-22 20:42:39 -0300

    Ajusto rutas de get user

--- src/routes.ts
+++ src/routes.ts
@@ -7,15 +7,14 @@ import { getFile, getFiles, postFile } from './controllers/files.controller';
 import { validateDownload } from './middlewares/validateDownload.middleware';
 import { validateAdmin } from './middlewares/validateAdmin.middleware';
 import { validateUpload } from './middlewares/validateUpload.middleware';
+import { getUsers } from './controllers/users.controller';
 
 const router = express.Router();
 
 export const routes = (app: any) => {
   router.post("/login", validateLogin, (req: Request, res: Response) => login(req, res));
 
-  router.get("/user", verifyToken, (req: Request, res: Response) => {
-    res.status(200).json({'User': req.user.surname});
-  });
+  router.get("/user", verifyToken, (req: Request, res: Response) => getUsers(req, res));
 
   router.post("/user", verifyToken, (req: Request, res: Response) => {
     res.status(200).json({'User': req.user.surname});
