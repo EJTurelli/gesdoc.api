28b5254d73a499e1a72c80468296c362e70ed989
Ernesto Turelli <ejturelli@yahoo.com>
2023-08-22 20:23:45 -0300

    Agrego middleware que valida la información necesaria del req para upload

---
+++ src/middlewares/validateUpload.middleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+var is = require('type-is');
+import * as yup from 'yup';
+
+const loginSchema = yup.object({
+  query: yup.object({
+    cuil: yup.string().min(11).max(11).required()
+  }),
+});
+
+export const validateUpload = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    if (!is(req, ['multipart'])) {
+      return res.status(403).send({message: 'Please provide a file'});
+    }
+
+    await loginSchema.validate({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+    return next();
+  } catch (err: any) {
+      return res.status(500).json({ message: err.message });
+  }
+};
+
+  
