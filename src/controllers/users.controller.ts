f52cfd157055de8d665d0f86db666683d69060e3
Ernesto Turelli <ejturelli@yahoo.com>
2023-08-23 09:31:03 -0300

    Agrego filtro en el controller de getusers

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,10 +1,29 @@
 import { Request, Response } from 'express';
-import { findAllActiveUsers } from "../services/user.service";
+import { findAllUsers } from "../services/user.service";
+import { IUserSearch } from '../interfaces/user.interface';
 
 export const getUsers = async (req: Request, res: Response) => {
 
     try {
-        const users = await findAllActiveUsers();
+        const {
+            surname,
+            name,
+            cuil,
+            email,
+            status,
+            rol,
+        } = req.query;
+
+        const search: IUserSearch = {
+            surname: surname as string,
+            name: name as string,
+            cuil: cuil as string,
+            email: email as string,
+            status: status as string,
+            rol: rol as string
+        };
+
+        const users = await findAllUsers(search);
 
         res.status(200).send({'users': users});
         return;
