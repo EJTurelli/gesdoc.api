c5de712e198e8ecc52b43548273af2419c6c8335
Ernesto Turelli <ejturelli@yahoo.com>
2023-09-04 19:56:45 -0300

    Ajusto servicio de encripción

--- src/services/encript.service.ts
+++ src/services/encript.service.ts
@@ -1,18 +1,30 @@
 import bcrypt from 'bcrypt';
 
-export const getFileHash = (name: string): Promise<string | void> => {
+const _createHash = (data: string, saltRonuds: number): Promise<string | void> => {
 
     return new Promise(async (resolve, reject) => {
 
         try {
-            const salt = await bcrypt.genSalt(process.env.FILES_SALT_ROUNDS);
-            const hash = await bcrypt.hash(name, salt);
+            const salt = await bcrypt.genSalt(saltRonuds);
+            const hash = await bcrypt.hash(data, salt);
            
             return resolve(hash);
         } catch (error) {
             return reject('Error cripto');
         }
     });
+}
+
+export const createHash = (data: string): Promise<string | void> => {
+    return _createHash(data, parseInt(process.env.SALT_ROUNDS));
+}
+
+export const createPassHash = (data: string): Promise<string | void> => {
+    return _createHash(data, parseInt(process.env.PASS_SALT_ROUNDS));
+}
+
+export const compareHash = (data: string, hash: string): Promise<boolean> => {
+    return bcrypt.compare(data, hash);
 }
 
 
