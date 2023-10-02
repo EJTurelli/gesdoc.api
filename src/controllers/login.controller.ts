0aafa8482406f0cb048024356b88e425f6f673d2
Ernesto Turelli <ejturelli@yahoo.com>
2023-10-02 18:12:36 -0300

    Agrego data del usuario en login

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,10 +1,11 @@
 
 import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
-import { findHashForActiveUserByCuil } from '../services/user.service';
+import { findActiveUserByCuil, findHashForActiveUserByCuil } from '../services/user.service';
 
 import dotenv from 'dotenv';
 import { compareHash } from '../services/encript.service';
+import { IUser } from '../interfaces/user.interface';
 dotenv.config();
 
 export const login = async (req: Request, res: Response) => {
@@ -15,8 +16,13 @@ export const login = async (req: Request, res: Response) => {
         if (hash) {
           const ok = await compareHash(password, hash); 
           if (ok) {
+            const user: IUser = await findActiveUserByCuil(req.body.cuil as string).catch ((err) => {
+              return res.status(500).json({ message: err });
+            }) as IUser;
+
             const token = jwt.sign({ cuil }, process.env.TOKEN_KEY, { expiresIn: "1h" });
-            res.status(200).json({ token });
+            const { id, ...rest } = user;
+            res.status(200).json({ ...rest, token });
             return;
           }        
         }
