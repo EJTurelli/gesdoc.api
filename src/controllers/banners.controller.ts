9372bba0bf1103467e3b421fc16093facfc63d04
Ernesto Turelli <ejturelli@yahoo.com>
2023-10-10 18:21:00 -0300

    Agrego validación de formato del nombre del banner

--- src/controllers/banners.controller.ts
+++ src/controllers/banners.controller.ts
@@ -13,22 +13,29 @@ export const getBanner = async (req: Request, res: Response) => {
             return;
         }
 
+
         for await (const file of files) {          
-            const date = new Date();
-            const dateFrom = new Date(parseInt(file.substring(0, 4)), parseInt(file.substring(4, 6))-1, parseInt(file.substring(6, 8)));
-            const dateTo = new Date(parseInt(file.substring(9, 13)), parseInt(file.substring(13, 15))-1, parseInt(file.substring(15, 17)), 23, 59, 59, 999);
 
-            if (dateFrom <= date && date <= dateTo) {
-                res.download(directoryPath + '/' + file, file, (err) => {
-                    if (err) {
-                      res.status(500).send({message: "Could not download the banner. " + err});
-                    }
-                  });
-                return;
+            // Valido el formato del nombre del banner
+            // debe ser AAAAMMDD-AAAAMMDD.extensión
+            // donde extensión puede ser: png o jpg o gif o webp o svg
+            const regexp = new RegExp(/^\d{8}-\d{8}.(png|jpg|gif|webp|svg)$/);
+            if (regexp.test(file)) {
+                const date = new Date();
+                const dateFrom = new Date(parseInt(file.substring(0, 4)), parseInt(file.substring(4, 6))-1, parseInt(file.substring(6, 8)));
+                const dateTo = new Date(parseInt(file.substring(9, 13)), parseInt(file.substring(13, 15))-1, parseInt(file.substring(15, 17)), 23, 59, 59, 999);
+    
+                if (dateFrom <= date && date <= dateTo) {
+                    res.download(directoryPath + '/' + file, file, (err) => {
+                        if (err) {
+                          res.status(500).send({message: "Could not download the banner. " + err});
+                        }
+                      });
+                    return;
+                }
             }
-
         };
-        
+
         const defaultBanner = 'default.png';
         res.download(directoryPath + '/' + defaultBanner, defaultBanner, (err) => {
             if (err) {
